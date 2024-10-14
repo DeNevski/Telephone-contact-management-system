a7ebc4e26d9135a19481d3d48ad08d8163bc3c62
DeNevski <denerlira@hotmail.com>
2024-10-14 17:47:36 -0300

    run

---
+++ run.py
@@ -0,0 +1,9 @@
+from src.binary_tree_structure.binary_search_tree import BinarySearchTree
+
+if __name__ == '__main__':
+
+    test = BinarySearchTree()
+    test.insert('Rafael', 34992068746)
+    test.insert('Pedro', 658464968448)
+    test.ascending_order()
+    #test.descending_order()
